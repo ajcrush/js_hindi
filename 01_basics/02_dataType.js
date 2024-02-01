2c97721b669b9b98bf144dd7bf005348cd90521c
Mohit kumar sharma <108133514+ajcrush@users.noreply.github.com>
2024-02-01 13:18:12 +0000

    datatypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,23 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3) // We ar using nodejd not broswer
+
+console.log(3+3)
+// Code readablity should be high
+let name = "Mohit"
+let age = 18
+let isLoggedIn = false
+
+// Number => 2 to power  53
+//bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null); // object
