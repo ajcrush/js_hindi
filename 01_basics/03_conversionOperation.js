78356c3b3e338e641311b3d39460090b4d79380e
Mohit kumar sharma <108133514+ajcrush@users.noreply.github.com>
2024-02-01 13:46:32 +0000

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,3 +21,28 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
+
+// **********************operation *******************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "hello"
+let str2 = " mohit"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1" + 2 )
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3
+num1=num2=num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
