310171525dc71199b29536756bd1a30ca3d9a119
Mohit kumar sharma <108133514+ajcrush@users.noreply.github.com>
2024-02-01 13:33:37 +0000

    conversion1

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = undefined
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33" => 33
+// "33abc" =>NaN (Not a Number)
+// zTrue => 1; false =>0
+let islogggedIn = ""
+let booleanIsLoggedIn = Boolean(islogggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// "" => faslse
+// "mohit" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
