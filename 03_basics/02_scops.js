50401a3ff14fb770427ab56ab5b5f684eb04432a
Mohit kumar sharma <108133514+ajcrush@users.noreply.github.com>
2024-02-06 12:10:27 +0000

    mini hoisting

--- 03_basics/02_scops.js
+++ 03_basics/02_scops.js
@@ -3,12 +3,51 @@ var c = 300
 if(true){
     let a = 10
     const b = 20
-    console.log("Inner :",a);
-    var c = 30
+    // console.log("Inner :",a);
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+
+function one(){
+    const username="Mohit"
+    function two(){
+        website="Youtube"
+        // console.log(username);
+    }
+    // console.log(website);
+    // two()
+}
+
+one()
+
+if(true){
+    const username="hitesh"
+    if(username == "hitesh"){
+        const website = " youtube"
+        // console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// +++++++++++++++++++++++++++ interesting ++++++++++++++++++++
+
+function addone(num) {
+    return num +1
+}
+addone(5)
+//we can use  the function or call it before delaration
+
+const addTwo = function(num){
+    return num+2
+}
+addTwo(5)
+//if we hold it on veriable then we cannot call the function before
+// declaration
+
