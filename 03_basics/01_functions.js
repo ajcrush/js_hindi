a499d35b6d7ce8e240f41f9ee039cc296fbbb022
Mohit kumar sharma <108133514+ajcrush@users.noreply.github.com>
2024-02-06 11:29:14 +0000

    funcrion with object and array

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -34,6 +34,34 @@ function loginUserMessage(username="Sam"){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
 
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,400,600));
+
+const user ={
+    username:"Mohit",
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+
+}
+// handleObject(user)
+handleObject({
+    username:"Sam",
+    price:99
+})
+
+const myNewArray =[100,200,300,400,55]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
