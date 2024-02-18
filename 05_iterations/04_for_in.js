35dfeb511848b096e30101144abbe617b7fb62cd
Mohit kumar sharma <108133514+ajcrush@users.noreply.github.com>
2024-02-18 12:08:04 +0000

    for_in

---
+++ 05_iterations/04_for_in.js
@@ -0,0 +1,27 @@
+//for in
+
+const myObject ={
+    js : 'javascript',
+    cpp : 'c++',
+    rb : 'ruby',
+    swift : 'swift by apple'
+
+}
+
+// for(const key in myObject){
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+const programming =["js","cpp","python","java"]
+// for (const key in programming){
+//     console.log(programming[key]);
+// }
+
+const map = new Map()
+map.set('IN',"INDIA")
+map.set('USA','UNITED STATE OF AMERICA')
+map.set('Fr','France')
+
+// for(const key in map){
+//     console.log(key);
+// }
