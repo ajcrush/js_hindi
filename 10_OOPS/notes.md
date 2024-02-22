7f5a457cb3f0eaf54ba77d776cb3b5f18aaa4249
Mohit kumar sharma <108133514+ajcrush@users.noreply.github.com>
2024-02-22 17:09:34 +0000

    OOPs this

--- 10_OOPS/notes.md
+++ 10_OOPS/notes.md
@@ -22,3 +22,8 @@
 -Encapsulation
 -Inheritance
 -Polymorphism
+
+## working mechanism of new
+-New keyword create new object everytime
+-constructor function called if new keyword
+-this keyword inject all the argument
--- 10_OOPS/oop.js
+++ 10_OOPS/oop.js
@@ -1,6 +1,39 @@
-const user ={
-    username:"Mohit",
-    loginCount :8,
-    signedIn :true
+// const user ={
+//     username:"Mohit",
+//     loginCount :8,
+//     signedIn :true,
+//     getDatabase :function(){
+//         // console.log("Connected to database");
+//         // console.log(`username: ${this.username}`);
+//         console.log(this);
+//     }
+// }
+
+
+
+// console.log(user.username);
+// console.log(user.getDatabase());
+// console.log(this);
+
+
+// const promiseOne = new Promise()
+// const date = new Date()
+// console.log(date);
+
+function user(username , loggingCount ,isLoggedIn){
+    this.username = username
+    this.loggingCount = loggingCount
+    this.isLoggedIn = isLoggedIn
+    this.greeting = function(){
+        console.log(`welcome : ${this.username}`);
+    }
+
+    return this
+
 }
-console.log(user.username);
+const userOne =new user('mohit',12,true)
+const userTwo =new user('chaiaurcode',11,false)
+// console.log(userOne);
+// console.log(userTwo);
+// console.log(userTwo.greeting());
+console.log(userOne.constructor);
