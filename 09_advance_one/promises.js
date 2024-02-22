18b5ff5ed4b31e20556ef794a2a7481acbcea30e
Mohit kumar sharma <108133514+ajcrush@users.noreply.github.com>
2024-02-22 10:12:57 +0000

    promise end

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -57,3 +57,48 @@ promiseFour
     console.log(error);
 })
 .finally(()=> console.log("The promise is either resolved or rejected"))
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(() => {
+        let error = true
+        if(!error){
+            resolve({username:"javascript",password:"123"})
+        }
+        else{
+            reject('ERROR : JS went wrong')
+        }
+    }, 1000);
+})
+
+async function  consumePromiseFive(){
+    try {
+        const response = await promiseFive
+    console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumePromiseFive()
+
+
+// async function getAllUsers(){
+//     try{
+        
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await response.json()
+//     console.log(data);
+//     }catch(error){
+//         console.log(error);
+//     }
+    
+// }
+
+// getAllUsers()
+
+
+fetch('https://api.github.com/users/ajcrush')
+.then((response)=>{
+    return response.json()
+})
+.then((data) =>{ console.log(data);})
+.catch((error) => console.log(error));
